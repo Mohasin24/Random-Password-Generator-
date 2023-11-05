96bcf08473c8a2958aeca1f333fe9fee52abd90a
Mohasin Patel <mohasinpatel313@gmail.com>
2023-11-05 21:24:57 +0530

    v2

--- scripts/index.js
+++ scripts/index.js
@@ -5,19 +5,16 @@ const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+=-{[]}/?|"
+const passwordLen = 12;
 
 const allChar = upperCase + lowerCase + numbers + symbols;
 
-function generatePassword()
-{   
+function generatePassword() {
     let password = "";
-    
-    password += upperCase[Math.floor(Math.random() * upperCase.length)] 
-                    + lowerCase[Math.floor(Math.random() * lowerCase.length)]
-                    + numbers[Math.floor(Math.random() * numbers.length)]
-                    + symbols[Math.floor(Math.random() * symbols.length)];
 
-    while(password.length<12)
+    password += upperCase[Math.floor(Math.random() * upperCase.length)] + lowerCase[Math.floor(Math.random() * lowerCase.length)] + numbers[Math.floor(Math.random() * numbers.length)] + symbols[Math.floor(Math.random() * symbols.length)];
+
+    while (password.length<passwordLen)
     {
         password += allChar[Math.floor(Math.random() * allChar.length)]
     }
@@ -25,16 +22,15 @@ function generatePassword()
     passwordBox.value = password;
 }
 
-copyBtn.addEventListener("click", ()=>{
-    console.log(passwordBox.value)
-    if(passwordBox.value !== '')
-    {
-        passwordBox.select();
-        document.execCommand("copy");
-        passwordBox.value = "Copied!";
-    }
-    else{
-        alert("Please generate the password!");
-    }
-    
-})
+copyBtn.addEventListener("click", ()=> {
+
+        if (passwordBox.value !== '') {
+            passwordBox.select();
+            navigator.clipboard.writeText(passwordBox.value);
+            passwordBox.value = "Copied!";
+
+        } else {
+            alert("Please generate the password!");
+        }
+
+    })
